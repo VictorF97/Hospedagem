b990fdb093fd38493185232e5b05bd2a5dbb4740
VictorF97 <franca1997victor@gmail.com>
2024-10-07 18:49:43 -0300

    first commit

---
+++ Models/Reserva.cs
@@ -0,0 +1,58 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Console.WriteLine("Hospedes cadastrados com sucesso!");
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Suíte não suporta a capacidade de hospedes!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            if(Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m;
+            }
+
+            return valor;
+        }
+    }
+}
